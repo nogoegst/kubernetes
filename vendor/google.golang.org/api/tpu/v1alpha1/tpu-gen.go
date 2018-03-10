24b198ea72b39f65a5595961e96b01541254b90b
Ivan Markin <sw@nogoegst.net>
2018-03-10 14:47:13 +0000

    Initial vendor conversion to dep
    Signed-off-by: Ivan Markin <sw@nogoegst.net>

--- vendor/google.golang.org/api/tpu/v1alpha1/tpu-gen.go
+++ vendor/google.golang.org/api/tpu/v1alpha1/tpu-gen.go
@@ -7,7 +7,7 @@
 //   import "google.golang.org/api/tpu/v1alpha1"
 //   ...
 //   tpuService, err := tpu.New(oauthHttpClient)
-package tpu
+package tpu // import "google.golang.org/api/tpu/v1alpha1"
 
 import (
 	"bytes"
